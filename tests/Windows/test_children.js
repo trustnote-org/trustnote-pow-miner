2a0f93d6fee06b1a0d862b5fd0463af4eb565c6b
liuqixing <liuqixing@gmail.com>
2018-09-25 18:54:48 +0800

    Added support for Windows platform

--- tests/Windows/test_children.js
+++ tests/Windows/test_children.js
@@ -1,5 +1,5 @@
 const _child_process	= require( 'child_process' );
-const _children		= require( '../js/children.js' );
+const _children		= require( '../../js/children.js' );
 
 
 const child		= _child_process.exec( "node -e 'while (true);'", function (){} );
