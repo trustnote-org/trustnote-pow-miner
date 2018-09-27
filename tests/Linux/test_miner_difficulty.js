793c8ccc5a644571914766db045b1a2faa133aa5
liuqixing <liuqixing@gmail.com>
2018-09-27 15:43:41 +0800

    Fixed a bug about _objMinerLibrary in file CTrustMinerLibrary.js

--- tests/Linux/test_miner_difficulty.js
+++ tests/Linux/test_miner_difficulty.js
@@ -8,3 +8,48 @@ const _miner	= require( '../../js/index.js' );
  */
 let nDifficulty = _miner.difficulty256HexToUInt32( "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" );
 console.log( `nDifficulty value : ${ nDifficulty }` );
+
+
+
+//
+//	calculate next difficulty
+//
+let nAverageDifficulty	= nDifficulty;
+let nTimeUsed		= 1000;
+let nTimeStandard	= 2400;
+_miner.calculateNextDifficulty
+(
+	nAverageDifficulty,
+	nTimeUsed,
+	nTimeStandard,
+	function( err, oData )
+	{
+		//
+		//	oData
+		//	{ difficulty : uNextDifficulty }
+		//
+		if ( err )
+		{
+			return console.error( err );
+		}
+
+		if ( oData &&
+			'object' === typeof oData )
+		{
+			if ( oData.hasOwnProperty( 'difficulty' ) &&
+				'number' === typeof oData.difficulty &&
+				oData.difficulty > 0 )
+			{
+				console.log( `new difficulty value after ${ nAverageDifficulty } is : ${ oData.difficulty }` );
+			}
+			else
+			{
+				console.log( `calculateNextDifficulty callback :: invalid value .difficulty` );
+			}
+		}
+		else
+		{
+			console.log( `calculateNextDifficulty callback :: invalid oData object` );
+		}
+	}
+);
