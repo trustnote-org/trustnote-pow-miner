8af8fd98685a31acf562fa8b5f54bb29e8f8005f
liuqixing <liuqixing@gmail.com>
2018-11-29 19:31:20 +0800

    Added release.Windows.bat

--- c/release.Windows.bat
+++ c/release.Windows.bat
@@ -1,12 +1,12 @@
-del "../lib/Windows/miner.Windows.x64.debug.dll"
-del "../lib/Windows/miner.Windows.x64.release.dll"
+del "..\lib\Windows\miner.Windows.x64.debug.dll"
+del "..\lib\Windows\miner.Windows.x64.release.dll"
 
 cl.clean.bat
 cl.dll.Windows.x64.debug.bat
-copy "output/miner.Windows.x64.debug.dll" "../lib/Windows/miner.Windows.x64.debug.dll"
+copy "output\miner.Windows.x64.debug.dll" "..\lib\Windows\miner.Windows.x64.debug.dll"
 
 cl.clean.bat
 cl.dll.Windows.x64.release.bat
-copy "output/miner.Windows.x64.release.dll" "../lib/Windows/miner.Windows.x64.release.dll"
+copy "output\miner.Windows.x64.release.dll" "..\lib\Windows\miner.Windows.x64.release.dll"
 
 cl.clean.bat
