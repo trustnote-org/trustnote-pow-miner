48bf058f29801dac75df7a67a0894932446b468d
liuqixing <liuqixing@gmail.com>
2018-11-29 19:27:45 +0800

    Added release.Windows.bat

---
+++ c/release.Windows.bat
@@ -0,0 +1,11 @@
+del ../lib/Windows/miner.*
+
+cl.clean.bat
+cl.dll.Windows.x64.debug.bat
+copy output/miner.Windows.x64.debug.dll ../lib/Windows/miner.Windows.x64.debug.dll
+
+cl.clean.bat
+cl.dll.Windows.x64.release.bat
+copy output/miner.Windows.x64.release.dll ../lib/Windows/miner.Windows.x64.release.dll
+
+cl.clean.bat
