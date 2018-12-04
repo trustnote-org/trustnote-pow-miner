765e72218b6352eada5ae9cc54a3195e2b6dc4ab
liuqixing <liuqixing@gmail.com>
2018-12-04 19:33:09 +0800

    Updated test_pow_check_pow.js

--- tests/test_pow_check_pow.js
+++ tests/test_pow_check_pow.js
@@ -7,13 +7,13 @@ const _miner	= require( '../js/index.js' );
 
 
 //let bufInputHeader	= Buffer.alloc( 140 );
-let sInputHeaderHex		= 'f93c6f10ded14bc3d2351389bdf8aa50aed5e1ea2182cc9e031a2c75f05ccec9ac96251ec756ad5dfc75935f05a02f5f48cdf5ad0a090830dbb78cbebb198d664d66fd93d3c04184af48a5ad093a9f79fdb07888875a1d3b0aab54993becd9edc75ef252447cb2da6b99c9df021007cb06463a97382c4776dfa76aa34dbc5c39d67428e4fc9dc99cb736db4b';
+let sInputHeaderHex		= '0e8d2f065b670eed8300e6c199ecb7ede00934c62fe0c18db0b345849623a0b64d8ca4916eed6e0417c678e56203aaaae986563402e45ca54bb394cbdf09bc8820953b223e7323e18e91d2c34ae8387daacd906116f8f9ff2e4e6248da78f7c1f31acce34fb4a9b60abd4f5753076ead38ac064d871fda92affcd6a3785a4d847c83d0965ddb43604384a103';
 let bufInputHeader		= Buffer.from( sInputHeaderHex, 'hex' );
 
 
-let uBits		= 528548564;	//_miner.getBitsByTarget( "007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" );
-let nActualNonce	= 1573;
-let sActualHashHex	= '008b9d227adec19f9c954d45d786c81d109b6c523900613d9bc7f257419ecade';
+let uBits		= 523973461;	//_miner.getBitsByTarget( "007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" );
+let nActualNonce	= 54;
+let sActualHashHex	= '00082504848696bddcead41792d1695e7066af24434ab885f1ad40f06e42a7ef';
 
 _miner.checkProofOfWork
 (
